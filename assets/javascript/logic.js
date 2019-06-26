a1ee8d127fd05c74dcddca7bd2d2edffeca1dbb1
wrsmall <william.r.small89@gmail.com>
2019-06-26 16:08:31 -0400

    everything is working

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,49 +1,74 @@
 var Choices = ["baseball", "hockey", "bowling", "tennis", "curling"];
-function getGif() {
-    for (i = 0; i < Choices.length; i++) {
-        var a = $("<button>");
-        a.addClass("answer1 text-center m-2 h5 rounded mycolor w-25 center");
-        a.attr("data-name", Choices[i]);
-        a.text(Choices[i]);
-        $("#button-choices").append(a);
 
-    };
-    $("#button-choices").on("click", ".answer1", ajax);
+function getGif() {
+
+  $("#buttons").empty();
+
+  for (i = 0; i < Choices.length; i++) {
+    var a = $("<button>");
+    a.addClass("answer1 text-center m-2 h5 rounded mycolor w-25 center");
+    a.attr("data-name", Choices[i]);
+    a.text(Choices[i]);
+    $("#buttons").append(a);
+
+  };
 };
-function ajax() {
+  $("#buttons").on("click", ".answer1", ajax);
+
+  function ajax() {
     $("#images").empty();
     var userSearch = $(this).attr("data-name");
     var key = "Nw1A45YOy3toCl9PIaEKtCT6863Knc9m";
     console.log(userSearch);
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Nw1A45YOy3toCl9PIaEKtCT6863Knc9m&q=" + userSearch + "&limit=10&rating=g";
-
     $.ajax({
-        url: queryURL,
-        method: "GET"
+      url: queryURL,
+      method: "GET"
     }).then(function (response) {
-        console.log(response);
-        var gifs = response.data;
+      console.log(response);
+      var gifs = response.data;
 
-        for (var i = 0; i < gifs.length; i++) {
-            var topicDiv = $("<div class='float-left size w-25 m-3'>");
-            var p = $("<p>");
-            p.text(gifs[i].rating);
-            var p = $("<p class='text-center'>").text("Rating: " + gifs[i].rating);
-            var topicImage = $("<img>")
+      for (var i = 0; i < gifs.length; i++) {
+        var gifDiv = $("<div class='float-left size w-25 m-3'>");
+        var p = $("<p>");
+        p.text(gifs[i].rating);
+        var p = $("<p class='text-center'>").text("Rating: " + gifs[i].rating);
+        var gifImage = $("<img>")
 
-            // add states of animate and still which will be toggled 
-            topicImage.attr("src", gifs[i].images.fixed_height_still.url);
-            topicImage.attr("data-still", gifs[i].images.fixed_height_still.url);
-            topicImage.attr("data-animate", gifs[i].images.fixed_height.url)
-            topicImage.attr("data-state", "still")
-            topicImage.addClass("gif size");
-            topicDiv.append(topicImage);
-	 			// rating is appended to the div below the gif
-	 		topicDiv.append(p); 			
-	 			// new images will be placed at the beginning (top) of the containing gif area
-	 		$("#images").prepend(topicDiv);
+        gifImage.attr("src", gifs[i].images.fixed_height_still.url);
+        gifImage.attr("data-still", gifs[i].images.fixed_height_still.url);
+        gifImage.attr("data-animate", gifs[i].images.fixed_height.url)
+        gifImage.attr("data-state", "still")
+        gifImage.addClass("gif size");
+        gifDiv.append(gifImage);
+        gifDiv.append(p);
+        $("#images").prepend(gifDiv);
 
-        };
+      };
+    
+      $(".gif").on("click", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
     });
-};
+  };
+    
+
+  $("#add").on("click", function (event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var sport = $("#added-sport").val().trim();
+
+    // Adding movie from the textbox to our array
+    Choices.push(sport);
+    getGif();
+  });
+
+
 getGif();
